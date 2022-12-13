c7b5b478b3ed9805f40700a9077876e9f74fc815
Your Name <you@example.com>
2022-12-13 10:31:47 -0800

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,13 +23,13 @@ void time_table(void)
 					_putchar(k + '0');
 				}
 				else if (k >= 10)
-				}
+				
 					_putchar(',');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
-				}
 			}
-			_putchar('\n');
 		}
+			
+			_putchar('\n');
 }
