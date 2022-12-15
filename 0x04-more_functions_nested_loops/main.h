75083a4a1c1cf18fa52acbe0095e35f6b6307838
Your Name <you@example.com>
2022-12-15 13:11:48 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+
+
+
+#define MAIN_H
+
+
+
+/*
+*
+*
+*
+* File: main.h
+*
+*
+*
+*
+*
+* Desc: Header file containing declartions for all function
+*
+*/
+
+int _isupper(int c)
+
+
+#endif
